967be56d1622aa91d061e74c333ce83a273dfea9
a1div0 <a.a.klenov@ya.ru>
2020-04-02 23:12:31 +0300

    Создаются таблицы и процедуры List

--- output.sql
+++ output.sql
@@ -1,1 +1,91 @@
-UsersCategories
+IF object_id('[Entity].[Users]', 'U') IS NOT NULL
+    DROP TABLE [Entity].[Users]
+GO
+
+CREATE TABLE [Entity].[Users] (
+    [user_id] BIGINT IDENTITY(1,1) NOT NULL,
+    [user_name] NVARCHAR(255) NOT NULL
+    [user_email] NVARCHAR(255) NOT NULL
+    [user_ext_id] NVARCHAR(255) NULL
+    [user_oauth_service_name] NVARCHAR(255) NOT NULL
+    [is_delete] BIT NOT NULL
+)
+GO
+
+IF object_id('[Entity].[UserList]', 'P') IS NOT NULL
+    DROP PROCEDURE [Entity].[UserList]
+GO
+
+CREATE PROCEDURE  [Entity].[UserList]
+AS
+
+    -- здесь будет проверка прав --
+
+    SET NOCOUNT ON;
+
+    SELECT
+        t.[user_id] [user_id]
+        t.[user_name] [user_name]
+        t.[user_email] [user_email]
+        t.[user_ext_id] [user_ext_id]
+        t.[user_oauth_service_name] [user_oauth_service_name]
+    FROM
+        [Entity].[Users] AS t
+    WHERE
+        t.is_delete = 0
+    ;
+RETURN
+GO
+
+IF object_id('[Entity].[Categories]', 'U') IS NOT NULL
+    DROP TABLE [Entity].[Categories]
+GO
+
+CREATE TABLE [Entity].[Categories] (
+    [category_id] BIGINT IDENTITY(1,1) NOT NULL,
+    [category_parent_id] BIGINT NOT NULL,
+    [category_is_folder] BIT NOT NULL,
+    [project_id] BIGINT NOT NULL,
+    [category_name] NVARCHAR(255) NOT NULL
+    [category_is_minus] BIT NOT NULL
+    [category_sort] REAL NOT NULL
+    [category_img_url] NVARCHAR(MAX) NOT NULL
+    [category_visible] BIT NOT NULL
+    [last_hand_user_id] BIGINT NOT NULL,
+    [is_delete] BIT NOT NULL
+)
+GO
+
+IF object_id('[Entity].[CategoryList]', 'P') IS NOT NULL
+    DROP PROCEDURE [Entity].[CategoryList]
+GO
+
+CREATE PROCEDURE  [Entity].[CategoryList]
+    @user_id BIGINT
+    ,@project_id BIGINT
+    ,@category_parent_id BIGINT
+AS
+
+    -- здесь будет проверка прав --
+
+    SET NOCOUNT ON;
+
+    SELECT
+        t.[category_id] [category_id]
+        t.[category_parent_id] [category_parent_id]
+        t.[category_is_folder] [category_is_folder]
+        t.[category_name] [category_name]
+        t.[category_is_minus] [category_is_minus]
+        t.[category_sort] [category_sort]
+        t.[category_img_url] [category_img_url]
+        t.[category_visible] [category_visible]
+    FROM
+        [Entity].[Categories] AS t
+    WHERE
+        t.is_delete = 0
+        AND t.project_id = project_id
+        AND t.category_parent_id = category_parent_id
+    ;
+RETURN
+GO
+
--- sqlgo.go
+++ sqlgo.go
@@ -7,7 +7,8 @@ package main
 import (
     "fmt" // пакет для форматированного ввода вывода
     "os"
-    //"strings"
+    "strings"
+    "strconv"
     "encoding/json"
 )
 
@@ -20,8 +21,10 @@ type TableDescription struct {
     Scheme string
     Table string
     OneRow string
-    History bool
-    Hierarhy bool
+    UseHistory bool
+    UseHierarhy bool
+    UseProjectId bool
+    UseUserId bool
     Cols []ColumnDescription
 }
 
@@ -83,7 +86,24 @@ func main() {
     return
 }
 
+// 2000 --> 8
+// 2005 --> 9
+// 2008 --> 10
+// 2012 --> 11
+// 2014 --> 12
+// 2016 --> 13
+// 2017 --> 14
+// 2019 --> 15
+
 func mssql_generate_sql(cfg *Configuration, file_name string) (error) {
+
+    ver, err := strconv.ParseUint(cfg.SqlGoFor.Version, 10, 64)
+    if (err != nil) {
+        return fmt.Errorf("Version '%s' is not integer! Please enter the version number, for example '2012'.", cfg.SqlGoFor.Version)
+    }
+    if (ver < 2012) {
+        return fmt.Errorf("Version '%s' is not support! Minimal is 2012.", cfg.SqlGoFor.Version)
+    }
 
     file, err := os.Create(file_name)
     if (err != nil) {
@@ -92,27 +112,27 @@ func mssql_generate_sql(cfg *Configuration, file_name string) (error) {
     defer file.Close()
 
     for _, table := range cfg.Tables {
-        err = mssql_create_table(file, &table)
+        err = mssql_create_table(file, &table, ver)
         if (err != nil) {
             return err
         }
 
-        err = mssql_storage_list(file, &table)
+        err = mssql_procedure_list(file, &table, ver)
         if (err != nil) {
             return err
         }
 
-        err = mssql_storage_merge(file, &table)
+        err = mssql_procedure_merge(file, &table, ver)
         if (err != nil) {
             return err
         }
 
-        err = mssql_storage_delete(file, &table)
+        err = mssql_procedure_delete(file, &table, ver)
         if (err != nil) {
             return err
         }
 
-        err = mssql_storage_history(file, &table)
+        err = mssql_procedure_history(file, &table, ver)
         if (err != nil) {
             return err
         }
@@ -121,25 +141,131 @@ func mssql_generate_sql(cfg *Configuration, file_name string) (error) {
     return nil
 }
 
-func mssql_create_table(f *os.File, table *TableDescription) (error) {
+func mssql_create_table(f *os.File, table *TableDescription, ver uint64) (error) {
 
-    f.WriteString(table.Table)
+    var nullable string
+
+    TableName := fmt.Sprintf("[%s].[%s]", table.Scheme, table.Table)
+    one_row := strings.ToLower(table.OneRow)
+
+    if (ver < 2016) {
+
+        fmt.Fprintf(f, "IF object_id('%s', 'U') IS NOT NULL\n", TableName)
+        fmt.Fprintf(f, "    DROP TABLE %s\n", TableName)
+        fmt.Fprintf(f, "GO\n\n")
+
+    } else {
+        fmt.Fprintf(f, "DROP TABLE IF EXISTS %s\n", TableName)
+        fmt.Fprintf(f, "GO\n\n")
+    }
+
+    fmt.Fprintf(f, "CREATE TABLE %s (\n", TableName)
+
+    fmt.Fprintf(f, "    [%s_id] BIGINT IDENTITY(1,1) NOT NULL,\n", one_row)
+
+    if (table.UseHierarhy == true) {
+        fmt.Fprintf(f, "    [%s_parent_id] BIGINT NOT NULL,\n", one_row)
+        fmt.Fprintf(f, "    [%s_is_folder] BIT NOT NULL,\n", one_row)
+    }
+
+    if (table.UseProjectId != false) {
+        fmt.Fprintf(f, "    [project_id] BIGINT NOT NULL,\n")
+    }
+
+    for _, col := range table.Cols {
+
+        if (col.Nullable == true) {
+            nullable = "NULL"
+        } else {
+            nullable = "NOT NULL"
+        }
+
+        fmt.Fprintf(f, "    [%s_%s] %s %s\n", one_row, col.Name, strings.ToUpper(col.Type), nullable)
+    }
+
+    if (table.UseHistory != true) {
+        fmt.Fprintf(f, "    [last_hand_user_id] BIGINT NOT NULL,\n")
+    }
+    fmt.Fprintf(f, "    [is_delete] BIT NOT NULL\n")
+    fmt.Fprintf(f, ")\n")
+    fmt.Fprintf(f, "GO\n\n")
+
     return nil
 }
 
-func mssql_storage_list(f *os.File, table *TableDescription) (error) {
+func mssql_procedure_list(f *os.File, table *TableDescription, ver uint64) (error) {
+
+    TableName := fmt.Sprintf("[%s].[%s]", table.Scheme, table.Table)
+    ProcedureName := fmt.Sprintf("[%s].[%sList]", table.Scheme, table.OneRow)
+    one_row := strings.ToLower(table.OneRow)
+
+    if (ver < 2016) {
+
+        fmt.Fprintf(f, "IF object_id('%s', 'P') IS NOT NULL\n", ProcedureName)
+        fmt.Fprintf(f, "    DROP PROCEDURE %s\n", ProcedureName)
+        fmt.Fprintf(f, "GO\n\n")
+
+    } else {
+        fmt.Fprintf(f, "DROP PROCEDURE IF EXISTS %s\n", ProcedureName)
+        fmt.Fprintf(f, "GO\n\n")
+    }
+
+    fmt.Fprintf(f, "CREATE PROCEDURE  %s\n", ProcedureName)
+    term := ""
+    if (table.UseUserId != false) {
+        fmt.Fprintf(f, "    @user_id BIGINT\n")
+        term = ","
+    }
+    if (table.UseProjectId != false) {
+        fmt.Fprintf(f, "    %s@project_id BIGINT\n", term)
+        term = ","
+    }
+    if (table.UseHierarhy != false) {
+        fmt.Fprintf(f, "    %s@%s_parent_id BIGINT\n", term, one_row)
+        term = ","
+    }
+    fmt.Fprintf(f, "AS\n\n")
+    fmt.Fprintf(f, "    -- здесь будет проверка прав --\n\n")
+    fmt.Fprintf(f, "    SET NOCOUNT ON;\n\n")
+    fmt.Fprintf(f, "    SELECT\n")
+    fmt.Fprintf(f, "        t.[%s_id] [%s_id]\n", one_row, one_row)
+    if (table.UseHierarhy != false) {
+        fmt.Fprintf(f, "        t.[%s_parent_id] [%s_parent_id]\n", one_row, one_row)
+        fmt.Fprintf(f, "        t.[%s_is_folder] [%s_is_folder]\n", one_row, one_row)
+    }
+
+    for _, col := range table.Cols {
+        fmt.Fprintf(f, "        t.[%s_%s] [%s_%s]\n", one_row, col.Name, one_row, col.Name)
+    }
+
+    fmt.Fprintf(f, "    FROM\n")
+    fmt.Fprintf(f, "        %s AS t\n", TableName)
+    fmt.Fprintf(f, "    WHERE\n")
+    fmt.Fprintf(f, "        t.is_delete = 0\n")
+
+    if (table.UseProjectId != false) {
+        fmt.Fprintf(f, "        AND t.project_id = project_id\n")
+    }
+    if (table.UseHierarhy != false) {
+        fmt.Fprintf(f, "        AND t.%s_parent_id = %s_parent_id\n", one_row, one_row)
+    }
+
+    fmt.Fprintf(f, "    ;\n")
+    fmt.Fprintf(f, "RETURN\n")
+    fmt.Fprintf(f, "GO\n\n")
+
     return nil
 }
 
-func mssql_storage_merge(f *os.File, table *TableDescription) (error) {
+func mssql_procedure_merge(f *os.File, table *TableDescription, ver uint64) (error) {
     return nil
 }
 
-func mssql_storage_delete(f *os.File, table *TableDescription) (error) {
+func mssql_procedure_delete(f *os.File, table *TableDescription, ver uint64) (error) {
     return nil
 }
 
-func mssql_storage_history(f *os.File, table *TableDescription) (error) {
+func mssql_procedure_history(f *os.File, table *TableDescription, ver uint64) (error) {
     return nil
 }
 
